2a5f0410a4112fe399b4f1def4d27ee2e6bdd508
ijunseo <wer3308@gmail.com>
2024-12-26 14:01:14 +0900

    [Platinum V] Title: 이진 탐색 트리, Time: 3916 ms, Memory: 176176 KB -BaekjoonHub

---
+++ PyPy3/백준/Platinum/2957. 이진 탐색 트리/이진 탐색 트리.py
@@ -0,0 +1,42 @@
+import sys
+input = sys.stdin.readline
+dot = int(input())
+dotlist = [[None, None, None] for _ in range(dot + 1)]
+root = int(input())
+dotlist[root][2] = 1
+import bisect
+dotset = []
+bisect.insort(dotset, root)
+def find(nownum):
+  lft = bisect.bisect_left(dotset, nownum)
+  if lft == 0:
+    bigdot = dotset[0]
+    dotlist[bigdot][0] = nownum
+    dotlist[nownum][2] = dotlist[bigdot][2] + 1
+    bisect.insort(dotset, nownum)
+    return
+  elif lft == len(dotset):
+    smalldot = dotset[-1]
+    dotlist[smalldot][1] = nownum
+    dotlist[nownum][2] = dotlist[smalldot][2] + 1
+    bisect.insort(dotset, nownum)
+    return
+  else:
+    bigdot = dotset[lft]
+    smalldot = dotset[lft - 1]
+    if dotlist[bigdot][2] > dotlist[smalldot][2]:
+      dotlist[bigdot][0] = nownum
+      dotlist[nownum][2] = dotlist[bigdot][2] + 1
+      bisect.insort(dotset, nownum)
+    else:
+      dotlist[smalldot][1] = nownum
+      dotlist[nownum][2] = dotlist[smalldot][2] + 1
+      bisect.insort(dotset, nownum)
+  return
+ans = 0
+print(ans)
+for _ in range(dot - 1):
+  num = int(input())
+  find(num)
+  ans += dotlist[num][2] - 1
+  print(ans)
