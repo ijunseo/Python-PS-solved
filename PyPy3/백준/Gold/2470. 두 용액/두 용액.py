90441236631455f3121d4428db2f65228c18dfd3
ijunseo <wer3308@gmail.com>
2024-12-26 13:52:56 +0900

    [Gold V] Title: 두 용액, Time: 120 ms, Memory: 126672 KB -BaekjoonHub

---
+++ PyPy3/백준/Gold/2470. 두 용액/두 용액.py
@@ -0,0 +1,32 @@
+import math
+import sys
+
+input = sys.stdin.readline
+num = int(input())
+numlist = list(map(int, input().split()))
+numlist.sort()
+minabs = math.inf
+ans = []
+start, end = 0, num - 1
+try:
+	while True:
+		if start == end:
+			break
+		nowgap = numlist[start] + numlist[end]
+		if nowgap == 0:
+			ans = [start, end]
+			break
+		if abs(nowgap) < minabs:
+			ans = [start, end]
+			minabs = abs(nowgap)
+			#print(ans)
+			#print(minabs)
+		if nowgap < 0:
+			start += 1
+		elif nowgap > 0:
+			end -= 1
+	for i in ans:
+		print(numlist[i], end = ' ')
+except:
+	for i in ans:
+		print(numlist[i], end = ' ')
