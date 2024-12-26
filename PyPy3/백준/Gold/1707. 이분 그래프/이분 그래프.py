fa2097b9c3826b263a5fdc9d3cac302d81ecf7eb
ijunseo <wer3308@gmail.com>
2024-12-26 13:58:57 +0900

    [Gold IV] Title: 이분 그래프, Time: 744 ms, Memory: 187396 KB -BaekjoonHub

---
+++ PyPy3/백준/Gold/1707. 이분 그래프/이분 그래프.py
@@ -0,0 +1,47 @@
+import sys
+input = sys.stdin.readline
+testcase = int(input())
+import heapq
+import math 
+
+
+def findans(q):
+  while q:
+    if color[0] == 1:
+      break
+    nownum = heapq.heappop(q)
+    nowcolor = color[nownum]
+    for i in line[nownum]:
+      if color[i] == 0:
+        if nowcolor == color_1:
+          color[i] = color_2
+          heapq.heappush(q, i)
+        elif nowcolor == color_2:
+          color[i] = color_1
+          heapq.heappush(q, i)
+      else:
+       if color[i] == nowcolor:
+         color[0] = 1
+       else:
+         continue
+for _ in range(testcase):
+  dot, linenum = map(int, input().split())
+  color = [0] * (dot + 1)
+  line = [[] for _ in range(dot + 1)]
+  for i in range(linenum):
+    a, b = map(int, input().split())
+    line[a].append(b)
+    line[b].append(a)
+  color_1 = 1
+  color_2 = 2
+  que = []
+  for i in range(1, dot + 1):
+    if color[i] == 0:
+      color[i] = color_1
+      heapq.heappush(que, i)
+      findans(que)
+  #print(color)
+  if color[0] == 1:
+    print('NO')
+  else:
+    print('YES')
