0e232f00a49edafb356cc813b74656aff88bdcc6
juns <wer3308@gmail.com>
2025-01-09 17:44:47 +0900

    [Gold IV] Title: 트리의 지름, Time: 72 ms, Memory: 37912 KB -BaekjoonHub

--- 백준/Gold/1967. 트리의 지름/트리의 지름.py
+++ 백준/Gold/1967. 트리의 지름/트리의 지름.py
@@ -3,29 +3,27 @@ input = sys.stdin.readline
 sys.setrecursionlimit(10 ** 5)
 dot = int(input())
 line = [[] for _ in range(dot + 1)]
-par  = [False] * (dot + 1)
 while True:
     try:
         a, b, c = map(int, input().split())
         line[a].append((b, c))
         line[b].append((a, c))
-        par[a] = True
     except:
         break
-ans = 0
-def search(n, cst, st):
+ans = [0, 0]
+def search(n, cst):
     global wentlist
     global ans
     wentlist[n] = True
     for i in line[n]:
         if wentlist[i[0]] == True:
-            ans = max(cst, ans)
+            if ans[1] < cst:
+                ans = [n, cst]
             continue
         newcost = cst + i[1]
-        search(i[0], newcost, st)
-
-for i in range(1, dot + 1):
-    if par[i] == False:
-        wentlist = [False] * (dot + 1)
-        search(i, 0, i)
-print(ans)
+        search(i[0], newcost)
+wentlist = [False] * (dot + 1)
+search(1, 0)
+wentlist = [False] * (dot + 1)
+search(ans[0], 0)
+print(ans[1])
