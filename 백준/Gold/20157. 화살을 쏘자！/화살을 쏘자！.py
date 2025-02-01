7fe3137848dd841662feb3eb1d969a38da0c2b73
juns <wer3308@gmail.com>
2025-02-01 12:49:40 +0900

    [Gold V] Title: 화살을 쏘자!, Time: 212 ms, Memory: 134332 KB -BaekjoonHub

--- 백준/Gold/20157. 화살을 쏘자！/화살을 쏘자！.py
+++ 백준/Gold/20157. 화살을 쏘자！/화살을 쏘자！.py
@@ -1,28 +1,33 @@
 #基本操作
 import sys
 input = sys.stdin.readline
-import bisect
 
 #input 
 n = int(input())
 katamuki_1 = set()
 katamuki_2 = set()
-ka1 = []
-ka2 = []
+ka1 = {}
+ka2 = {}
 divpl = 0
 divmin = 0
 ans = 0
-lft1 = 0
-lft2 = 0
 for _ in range(n):
     x, y = map(int, input().split())
     if x > 0:
-        bisect.insort(ka1, y/x)
-        katamuki_1.add(y/x)
+        now = y/x
+        if now in katamuki_1:
+            ka1[now] += 1
+        else:
+            katamuki_1.add(now)
+            ka1[now] = 1
 
     elif x < 0:
-        bisect.insort(ka2, y/x)
-        katamuki_2.add(y/x)
+        now = y/x
+        if now in katamuki_2:
+            ka2[now] += 1
+        else:
+            katamuki_2.add(now)
+            ka2[now] = 1
 
     else:
         if y > 0:
@@ -30,18 +35,12 @@ for _ in range(n):
         else:
             divmin += 1
 
-            
-katamuki_1 = sorted(katamuki_1)
-katamuki_2 = sorted(katamuki_2)
 for i in katamuki_1:
-    rgt1 = bisect.bisect_right(ka1, i)
-    ans = max(ans, rgt1 - lft1)
-    lft1 = rgt1
+    ans = max(ans, ka1[i])
+
 
 for i in katamuki_2:
-    rgt2 = bisect.bisect_right(ka2, i)
-    ans = max(ans, rgt2 - lft2)
-    lft2 = rgt2
+    ans = max(ans, ka2[i])
 
 #output
-print(max(ans,divpl, divmin))
+print(max(ans, divpl, divmin))
