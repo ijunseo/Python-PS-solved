f6d6fa8c2e34cc9c89a2bf9e07665b3c17abe22c
juns <wer3308@gmail.com>
2025-03-14 14:44:19 +0800

    [Gold V] Title: 토마토, Time: 448 ms, Memory: 229528 KB -BaekjoonHub

---
+++ PyPy3/백준/Gold/7576. 토마토/토마토.py
@@ -0,0 +1,36 @@
+#基本操作
+import sys
+from collections import deque
+input = sys.stdin.readline
+
+#input
+q = deque([])
+m, n = map(int, input().split())
+lst = [list(map(int, input().split())) for _ in range(n)]
+for i in range(n):
+	for j in range(m):
+		if lst[i][j] == 1:
+			q.append([i, j, 0])
+llst = [[0] * m for _ in range(n)]
+ans = 0
+#solv
+dx = [1, -1, 0, 0]
+dy = [0, 0, 1, -1]
+while q:
+	nowr, nowc, cst = q.popleft()
+	for i in range(4):
+		nexr = nowr + dx[i]
+		nexc = nowc + dy[i]
+		if 0 <= nexr < n and 0 <= nexc < m:
+			if lst[nexr][nexc] == 0:
+				lst[nexr][nexc] = 1
+				nextcst = cst + 1
+				llst[nexr][nexc] = nextcst
+				q.append([nexr, nexc, nextcst])
+for i in range(n):
+	for j in range(m):
+		ans = max(llst[i][j], ans)
+		if lst[i][j] == 0:
+			print('-1')
+			sys.exit()
+print(ans)
