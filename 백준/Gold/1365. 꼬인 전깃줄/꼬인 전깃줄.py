61d3c8225a098c6b35704a3ddef239807338ed3f
juns <wer3308@gmail.com>
2025-04-07 23:31:05 +0900

    [Gold II] Title: 꼬인 전깃줄, Time: 124 ms, Memory: 125284 KB -BaekjoonHub

--- 백준/Gold/1365. 꼬인 전깃줄/꼬인 전깃줄.py
+++ 백준/Gold/1365. 꼬인 전깃줄/꼬인 전깃줄.py
@@ -5,41 +5,21 @@ import bisect
 
 #input 
 n = int(input())
-lst_1 = list(map(int, input().split()))
+lst = list(map(int, input().split()))
 for i in range(n):
-    lst_1[i] -= 1
+    lst[i] -= 1
 
-lst_2 = [0] * n
+sub_lst = []
 for i in range(n):
-    now = lst_1[i]
-    lst_2[n - 1 -  now] = n - 1 - i
-
-
-sub_lst_1 = []
-for i in range(n):
-    now = lst_1[i]
-    if not sub_lst_1:
-        sub_lst_1.append(now)
+    now = lst[i]
+    if not sub_lst:
+        sub_lst.append(now)
     else:
-        if now > sub_lst_1[-1]:
-            sub_lst_1.append(now)
+        if now > sub_lst[-1]:
+            sub_lst.append(now)
             continue
         else:
-            rgt = bisect.bisect_right(sub_lst_1, now)
-            sub_lst_1[rgt] = now
+            rgt = bisect.bisect_right(sub_lst, now)
+            sub_lst[rgt] = now
 
-# sub_lst_2 = []
-# for i in range(n):
-#     now = lst_2[i]
-#     if not sub_lst_2:
-#         sub_lst_2.append(now)
-#     else:
-#         if now > sub_lst_2[-1]:
-#             sub_lst_2.append(now)
-#             continue
-#         else:
-#             rgt = bisect.bisect_right(sub_lst_2, now)
-#             sub_lst_2[rgt] = now
-
-# print(n - max(len(sub_lst_1), len(sub_lst_2)))
-print(n - len(sub_lst_1))
+print(n - len(sub_lst))
