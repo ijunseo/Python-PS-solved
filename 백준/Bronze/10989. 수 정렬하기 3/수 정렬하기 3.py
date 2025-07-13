485f4d217b71e48da61927899d31edd8b5988420
juns <wer3308@gmail.com>
2025-07-13 21:26:26 +0900

    [Bronze I] Title: 수 정렬하기 3, Time: 2792 ms, Memory: 126400 KB -BaekjoonHub

--- 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.py
+++ 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.py
@@ -1,12 +1,12 @@
-from collections import defaultdict
 import sys
 input = sys.stdin.readline
 
-d = defaultdict(int)
+lst = [0] * 10001
 n = int(input())
 for _ in range(n):
     a = int(input())
-    d[a] += 1
-for i in sorted(d):
-    for _ in range(d[i]):
-        print(i)
+    lst[a] += 1
+for i in range(10001):
+    if lst[i]:
+        for _ in range(lst[i]):
+            print(i)
