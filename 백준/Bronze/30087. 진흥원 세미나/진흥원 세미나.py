ca940deea239b729c47276af6c4687df08ea1664
juns <wer3308@gmail.com>
2025-10-06 21:23:49 +0900

    [Bronze V] Title: 진흥원 세미나, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/30087. 진흥원 세미나/진흥원 세미나.py
@@ -0,0 +1,15 @@
+doc = {
+    "Algorithm": "204",
+    "DataAnalysis": "207",
+    "ArtificialIntelligence": "302",
+    "CyberSecurity": "B101",
+    "Network": "303",
+    "Startup": "501",
+    "TestStrategy": "105",
+}
+ 
+n = int(input())
+ 
+for _ in range(n):
+    q = input()
+    print(doc[q])
